56d479479e89abfa05c55b2c7e5ef2f674858425
Libra Aries Books <libra@localhost.localdomain>
2009-03-07 18:05:07 +0000

    try a hash function

--- hash-test.c
+++ hash-test.c
@@ -1,21 +1,32 @@
-#include <stdio.h>
-
+#include <stdio.h> 
 #define SIZE 251
 
 #define HASHSTART 5381
 
-static unsigned int hashadd(unsigned int h, unsigned char c) {
+static unsigned int hashadd(unsigned int h, unsigned int n) {
 	h += h << 5;
-	h ^= c;
+	h ^= n;
 	return h;
 }
 
 int main(void) {
-	int i, j, k;
-	int tally[SIZE];
+	int h, i, j, k;
+	long tally[SIZE];
 	unsigned char *p;
+
+	for (i = 0; i < SIZE; ++i)
+		tally[i] = 0;
 
 	for (j = 0; j < 100000; ++j) {
 		for (k = 0; k < 100; ++k) {
-			h = hashadd(HASHSTART, k);
-			for (p = (unsigned char *)&j; 
+			h = HASHSTART;
+			h = hashadd(h, k);
+			h = hashadd(h, j);
+			++tally[h % SIZE];
+		}
+	}
+
+	for (i = 0; i < SIZE; ++i) {
+		printf("%3d %ld\n", i, tally[i]);
+	}
+}
