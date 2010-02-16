91406c98a3df42833d202c6724e6e64bbfb2e820
Tim Goodwin <libresoftwaredesign@googlemail.com>
2010-02-16 12:06:12 +0000

    sorting test suite, again

--- test/run-all.sh
+++ test/run-all.sh
@@ -22,9 +22,55 @@ noparse() {
     check "$1" "expected $2 at column $3"
 }
 
-#while false; do
+run() {
+    target="$1"
+    echo $target
+    type=`sed -n 2p $target`
+    case $type in
+	chars|int) cp parse.h-$type parse.h ;;
+	*)
+	    echo "FAIL $target: bad type $type"
+	    fails=`expr $fails + 1`
+	    return 1
+	    ;;
+    esac
+    # Need better Makefiles?
+    rm -f emitter emitter.o parse.c parse.o harness harness.o
+    case $target in
+	emit/*)
+	    cp $target emitter.c
+	    make emitter
+	    ./emitter -o parse.c Makefile
+	    ;;
+	*)
+	    cp $target parse.pacc
+	    ;;
+    esac
+    make harness
+    t=`mktemp`
+    egrep '^check|^parse|^noparse' $target > $t
+    if test -s $t; then
+	# Turn off globbing for the duration, so 2*3 works as expected.
+	set -f
+	. $t
+	set +f
+	rm $t
+    else
+	echo "FAIL $target: no tests found"
+	fails=`expr $fails + 1`
+    fi
+}
+
+rm -f pacc/parse.pacc
+
+#for t in emit/mk-*.c pacc/*.pacc; do
+for t in pacc/*.pacc; do
+    run $t
+done
+
+while false; do
 #for target in emit/mk-regress0.c; do
-for target in emit/mk-*.c; do
+#for target in emit/mk-*.c; do
     echo $target
     rm -f emitter emitter.o parse.c parse.o harness harness.o
     cp $target emitter.c
@@ -43,9 +89,9 @@ for target in emit/mk-*.c; do
     rm $t
 done
 
-for target in pacc/*.pacc; do
-#for target in pacc/guard7.pacc; do
-    if [ $target = pacc/parse.pacc ]; then continue; fi
+while false; do
+#for target in pacc/*.pacc; do
+#for target in pacc/comment?.pacc; do
     echo $target
     cp $target parse.pacc
     cp parse.h-`sed -n 1s/..//p $target` parse.h
