27e12a9407cf939364953dc1595a2d77f6509bd3
Tim Goodwin <libresoftwaredesign@googlemail.com>
2010-11-13 20:41:45 +0000

    support automatic 0 values for query calls

--- sugar.c
+++ sugar.c
@@ -15,6 +15,9 @@
 
 static struct s_node *rules = 0;
 
+/* XXX only find_type needs this; must be a better way? */
+static struct s_node *top;
+
 /*
 The deblit (desugar bound literal) transform converts this tree:
 
@@ -62,6 +65,67 @@ static int isblit(struct s_node *n) {
     return n->type == lit && n->first;
 }
 
+/* XXX this is pretty grody, and kindof in the wrong place */
+char *find_type(char *name) {
+    struct s_node *p;
+
+    for (p = top->first->next; p; p = p->next)
+	if (strcmp(p->text, name) == 0)
+	    return p->first->text;
+
+    fatal3("rule not found: `", name, "'");
+    return 0; /* not reached */
+}
+
+/*
+The dequery (desugar query operator) transform changes
+    R <- x: Thing? { ... }
+    Thing :: t <- ...
+into
+    R <- x: ThingOpt { ... }
+    ThingOpt :: t <- a:Thing -> a / epsilon -> 0
+except with different names for the virtual rules. It (mostly) works
+because "0" is a member of both numeric and pointer types in C.
+*/
+
+static void dequery(struct s_node *n, const char *name, int c) {
+    char *xo, *thing, *typ;
+    int l;
+    struct s_node *p, *q, *s;
+
+    thing = n->first->first->text;
+
+    /* XXX to find the type of the called rule, we need to find the
+     * rule, and we haven't called resolve() yet. At this point, we
+     * don't even have a pointer that's of any use to find it! Darn,
+     * this is going to get messy.
+     */
+    typ = find_type(thing);
+    //fprintf(stderr, "thing is %s :: %s\n", thing, typ);
+
+    /* Create the name for ThingOpt. */
+    l = snprintf(0, 0, "%s:?:%d", name, c) + 1;
+    xo = malloc(l);
+    if (!xo) nomem();
+    snprintf(xo, l, "%s:?:%d", name, c);
+
+    /* generate the new rule */
+    p = s_kid(seq, s_text(expr, "0")); s = p;
+    p = s_both(expr, "a", s_text(ident, "a"));
+    p = cons(s_both(bind, "a", s_text(call, thing)), p); q = p;
+    p = s_new(seq); p->first = q; p->next = s; q = p;
+    p = s_new(alt); p->first = q; q = p;
+    p = s_text(type, typ); p->next = q; q = p;
+    p = s_text(rule, xo); p->first = q;
+    p->next = rules; rules = p;
+
+    /* replace "bind / rep / call" with "bind / call" */
+    n->first->text = xo;
+    n->first->type = call;
+    free(n->first->first);
+    n->first->first = 0;
+}
+
 /*
 The debind (desugar binding) transform creates a new rule for the right
 hand side of a non-rule binding.
@@ -76,6 +140,13 @@ static void debind(struct s_node *n, const char *name, int c) {
     char *x;
     int l;
     struct s_node *p, *r;
+
+    if (n->first && n->first->type == rep &&
+	n->first->text && strcmp(n->first->text, ",1") == 0 &&
+	n->first->first && n->first->first->type == call) {
+	dequery(n, name, c);
+	return;
+    }
 
     l = snprintf(0, 0, "%s:%s:%d", name, n->text, c) + 1;
     x = malloc(l);
@@ -192,6 +263,8 @@ static void walk(struct s_node *n, pred_fn_t pred, trans_fn_t transform) {
 
 void desugar(struct s_node *g) {
     struct s_node *p;
+
+    top = g; /* XXX for find_type */
 
     walk(g, &isblit, &deblit);
 
