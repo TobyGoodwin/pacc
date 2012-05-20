8fb83b1d2c2e486a9ce234eb8f7de9bf716789d5
Toby Goodwin <LibreSoftwareDesign@gmail.com>
2012-05-20 21:12:17 +0100

    add feed tests

---
+++ test/feed/run.sh
@@ -0,0 +1,51 @@
+#! /bin/sh
+
+# This code does not handle spacy filenames, nor should it.
+
+parse() {
+    exp="$1"
+    shift
+    echo ./feeder "$@"
+    r=`./feeder "$@" 2>&1`
+    check "$r" "parsed with value $exp"
+}
+export -f parse
+
+parse_exact() {
+    echo ./feeder "$1"
+    r=`./feeder "$1" 2>&1`
+    check "$r" "$2"
+}
+export -f parse
+
+end() {
+    echo ./feeder "$@"
+    r=`./feeder "$@" 2>&1`
+    check "$r" "unexpected end of input"
+}
+
+noparse() {
+    coord="$1"
+    shift
+    exp="$1"
+    shift
+    echo ./feeder "$@"
+    r=`./feeder "$@" 2>&1`
+    check "$r" "arg:$coord: expected $exp"
+}
+export -f noparse
+
+type() {
+    case $1 in
+	chars|int) echo cp parse.h-$1 parse.h; cp parse.h-$1 parse.h ;;
+	*) fail "bad type $1" ;;
+    esac
+    rm -f emitter emitter.o parse.c parse.o harness harness.o
+    make feeder || exit
+}
+export -f type
+
+target=$1
+echo cp $target parsefeed.pacc
+cp $target parsefeed.pacc
+script_from $target
